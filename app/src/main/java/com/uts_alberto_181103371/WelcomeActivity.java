9efbc9dff313caa3c11dbec2e2f2406d28c13ab4
AgustinusIrfandra <agustinusirfandra.com>
2022-04-25 22:27:51 +0700

    UTS-Alberto-181103371

---
+++ app/src/main/java/com/uts_alberto_181103371/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.uts_alberto_181103371;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
